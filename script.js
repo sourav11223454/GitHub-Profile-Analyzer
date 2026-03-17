d913b94aa5443ff0a5b2fe3e309bec9c6086a5dc
sourav11223454 <souravvs2255@gmail.com>
2026-03-17 15:29:13 +0530

    project started

---
+++ script.js
@@ -0,0 +1,5 @@
+// <!-- SEARCH-CARD -->
+//                 <div class="d-flex gap-2">
+//                     <input type="text" class="form-control rounded-pill" placeholder="Search city..." id="inputCity">
+//                     <button onclick="Search()" class="btn btn-dark rounded-pill">Search</button>
+//                 </div>
